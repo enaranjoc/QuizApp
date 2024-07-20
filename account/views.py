ef0191daa8b393dfccf9a9ee58c42c841a8dd222
Erick Naranjo <ariel99.en@gmail.com>
2024-07-19 23:29:04 -0500

    se agrego las vistas de registro, login y logout
    - se agrego las urls de las vistas de registro, login y logout

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+    redirect_authenticated_user = True  # Redirige al usuario a la página principal después de iniciar sesión
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')  # Redirige al usuario a la página de inicio de sesión después de cerrar sesión
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
